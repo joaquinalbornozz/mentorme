5ba9b409e9abd581b7811d7d024c1d882648788a
joaquinalbornozz <joaquinealbornoz021@gmail.com>
2024-12-10 16:54:53 -0300

    conexion_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mentorme/src/services/firebase_options.dart';
 import 'package:mentorme/src/pages/welcome_page.dart';
 import 'package:mentorme/src/routes/routes.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -6,7 +8,13 @@ import 'package:mentorme/src/utils/cartnotifier.dart';
 import 'package:provider/provider.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => CartNotifier(), // Proveedor para toda la app
